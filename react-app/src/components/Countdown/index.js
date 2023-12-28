2c22492951e5fad13c556c22f4eac9f4356ea09f
Eli <elixfoster@gmail.com>
2023-12-28 09:33:22 -0500

    finished

--- react-app/src/components/Countdown/index.js
+++ react-app/src/components/Countdown/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './Countdown.css'
+import React, { useState, useEffect } from "react";
+import "./Countdown.css";
 
 const Countdown = ({ targetDate }) => {
   const calculateTimeLeft = () => {
@@ -7,20 +7,27 @@ const Countdown = ({ targetDate }) => {
     const difference = new Date(targetDate) - now;
 
     if (difference > 0) {
-      const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
       const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
       return {
-        hours: String(hours).padStart(2, '0'),
-        minutes: String(minutes).padStart(2, '0'),
-        seconds: String(seconds).padStart(2, '0'),
+        totalHours: Math.floor(difference / (1000 * 60 * 60)),
+        days: String(days).padStart(2, "0"),
+        hours: String(hours).padStart(2, "0"),
+        minutes: String(minutes).padStart(2, "0"),
+        seconds: String(seconds).padStart(2, "0"),
       };
     } else {
       return {
-        hours: '00',
-        minutes: '00',
-        seconds: '00',
+        totalHours: 0,
+        days: "00",
+        hours: "00",
+        minutes: "00",
+        seconds: "00",
       };
     }
   };
@@ -38,18 +45,23 @@ const Countdown = ({ targetDate }) => {
   }, [timeLeft]);
 
   return (
-    <div className='countdown-wrapper'>
-      <div id='countdown-box-one'>
+    <div className="countdown-wrapper">
+      <div id="countdown-box-one">
+        <div>{timeLeft.days}</div>
+        <div>Days</div>
+      </div>
+      <div>:</div>
+      <div id="countdown-box-one">
         <div>{timeLeft.hours}</div>
         <div>Hours</div>
       </div>
       <div>:</div>
-      <div id='countdown-box-two'>
+      <div id="countdown-box-two">
         <div>{timeLeft.minutes}</div>
         <div>Minutes</div>
       </div>
       <div>:</div>
-      <div id='countdown-box-three'>
+      <div id="countdown-box-three">
         <div>{timeLeft.seconds}</div>
         <div>Seconds</div>
       </div>
--- react-app/src/components/SuccessModal/index.js
+++ react-app/src/components/SuccessModal/index.js
@@ -22,13 +22,14 @@ function SuccessModal() {
     }
   };
 
-  const targetDate = '2023-12-29T20:00:00';
+  const targetDate = '2023-12-29T19:00:00-05:00';
 
   return (
     <div className='success-wrapper'>
       <div id='success-box-one'>YOUR SEAT IS SAVED!</div>
       <i id='success-box-two'>And Your Email Is On The Way For The Link To Think & Grow Into A Millionaire</i>
-      <div id='success-box-three'>DECEMBER 29TH AND 30TH</div>
+      <div id='zoom-reminder'>Be Sure To Register on <span style={{ color: 'blue' }}>ZOOM</span> Using The Link in The Email</div>
+      <div id='success-box-three'>DECEMBER 29TH AND 30TH <span style={{ color: 'green' }}>@ 7PM EST</span></div>
       <Countdown targetDate={targetDate} />
     </div>
   );
